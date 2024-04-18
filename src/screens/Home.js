91e066602f3158a134a129db4a354e8cc09cc991
Shreyansh Raj <211230051@nitdelhi.ac.in>
2024-04-18 11:09:13 +0530

    Design Changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import bannerImg from "../images/banner.jpg";
 
 const Home = () => {
-    return (
-        <div>
-            Home is it
-        </div>
-    )
-}
+  const imgStyles = {
+    width: "100vw",
+    height: "90vh",
+    objectFit: "cover",
+    filter: "blur(2.5px)",
+  };
+  const containerStyles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    width: "100vw",
+  };
 
-export default Home
+  return (
+    <>
+      <div style={containerStyles}>
+        <img src={bannerImg} alt="Banner" style={imgStyles} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
