ffff5630e944dc8cb37942516d7cb6e144f22f91
Shreyansh Raj <211230051@nitdelhi.ac.in>
2024-04-18 09:26:27 +0530

    Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import UploadImage from './components/UploadImage';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./screens/Home"
+import Login from "./screens/Login"
+import Signup from "./screens/Signup"
 function App() {
   return (
     <div className="App">
-      <UploadImage />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
